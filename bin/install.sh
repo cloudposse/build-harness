4838dfb7ef0d0f5ec4a7621cd9c2200bd59bbffd
Igor Rodionov <goruha@gmail.com>
2017-01-19 16:45:12 +0600

    Added build-harness right way

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-master}
-export GITHUB_REPO="git@github.com:cloudposse/${BUILD_HARNESS_PROJECT}.git"
+export GITHUB_REPO="https://github.com/cloudposse/${BUILD_HARNESS_PROJECT}.git"
 
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
 	echo "Removing existing $BUILD_HARNESS_PROJECT"
