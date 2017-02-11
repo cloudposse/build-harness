e4fd4753a877544b83f2eb2f201e882577eded6d
Erik Osterman <e@osterman.com>
2017-02-10 19:21:42 -0800

    be more verbose

--- bin/install.sh
+++ bin/install.sh
@@ -4,8 +4,9 @@ export BUILD_HARNESS_BRANCH=${2:-master}
 export GITHUB_REPO="https://github.com/cloudposse/${BUILD_HARNESS_PROJECT}.git"
 
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
-	echo "Removing existing $BUILD_HARNESS_PROJECT"
+  echo "Removing existing $BUILD_HARNESS_PROJECT"
   rm -rf "$BUILD_HARNESS_PROJECT"
 fi
 
+echo "Cloning ${GITHUB_REPO}#${BUILD_HARNESS_BRANCH}..."
 git clone -b $BUILD_HARNESS_BRANCH $GITHUB_REPO
