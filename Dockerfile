29b17f437536aaa1e93d931b51a81954ba94a0d9
Erik Osterman <erik@cloudposse.com>
2018-05-01 11:26:06 -0700

    Add helmfile & chamber (#54)
    * add helmfile
    
    * Add chamber
    
    * add distro
    
    * install distro
    
    * use yq to rewrite yaml; do not templatize yaml
    
    * use https
    
    * fix docker image
    
    * concise errors
    
    * fix typo
    
    * install all dependencies
    
    * add ca-certificates
    
    * rename distro to packages
    
    * pin to release
    
    * add gettext for envsubst
    
    * disable go-deps due to 404
    
    * re-enable deps-dev
    
    * Upgrade helm
    
    * move helm version
    
    * add upsert
    
    * use official release
    
    * add timeout
    
    * require timeout
    
    * use short version
    
    * translate new line to comma
    
    * Update helm_toolbox.sh

--- Dockerfile
+++ Dockerfile
@@ -2,19 +2,23 @@ FROM golang:1.9.2-alpine3.7
 
 RUN apk update && \
     apk --update add \
-      jq \
-      git \
-      make \
-      curl \
       bash \
-      grep
+      ca-certificates \
+      coreutils \
+      curl \
+      git \
+      gettext \
+      grep \
+      jq \
+      make
 
 ADD ./ /build-harness/
 
-RUN cd /build-harness && \
-      make helm/install template/deps
+ENV INSTALL_PATH /usr/local/bin
 
 WORKDIR /build-harness
+
+RUN make -s chamber/install helm/install helmfile/install template/deps
 
 ENTRYPOINT ["/bin/bash"]
 
---
+++ bin/helm_toolbox.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+export TIMEOUT=3
+export STDOUT=${STDOUT:-/dev/null}
+
+# helper functions
+function info() { 
+	echo -e "\e[32mINFO:\e[0m $1"; 
+}
+
+function err() {
+	echo -e "\e[31mERROR:\e[0m $1" ; 
+	exit 1; 
+}
+
+function check() { 
+	command -v "$1" >/dev/null 2>&1 || err "$1 not installed!"; 
+}
+
+# wait for helm to become operational
+wait_for_helm() {
+	info "Waiting for helm tiller..."
+	while true; do
+		status=$(kubectl get pods -l app=helm -l name=tiller --show-all=false -o=custom-columns=STATUS:.status.phase --no-headers=true -nkube-system | tr '\n' ',')
+		info "Helm status: $status"
+		if [ "$status" = "Running" ]; then
+			break;
+		fi
+		sleep $TIMEOUT
+	done
+}
+
+function set_context() {
+  if [ -n "$KUBE_CONTEXT" ]; then
+	info "Using ${KUBE_CONTEXT} kube context"
+	kubectl config use-context ${KUBE_CONTEXT}
+  fi
+}
+
+function upsert() {
+	helm_version=$(helm version --client --short | grep -Eo "v[0-9]\.[0-9]\.[0-9]")
+	tiller_version=$(timeout $TIMEOUT helm version --server --short | grep -Eo "v[0-9]\.[0-9]\.[0-9]")
+
+    if [ -z "$tiller_version" ]; then
+      err "Unable to connect to helm server"
+    fi
+
+	if [ "$helm_version" != "$tiller_version" ]; then
+		info "Helm version: $helm_version, differs with tiller version: $tiller_version"
+		info "Upgrarding tiller to $helm_version"
+		helm init --upgrade --force-upgrade > $STDOUT
+		wait_for_helm
+		sleep 3
+		info "Helm version"
+		helm version --short | sed 's/^/  - /'
+	else
+		info "Helm version: $helm_version matches tiller version: $tiller_version."
+		info "Initializing helm client..."
+	 	helm init --client-only > $STDOUT
+	fi
+}
+
+if [ "$1" == "upsert" ]; then
+	check kubectl
+	check helm
+	check timeout
+	set_context
+	upsert
+else
+	err "Unknown commmand"
+fi
